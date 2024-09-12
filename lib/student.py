1020f156fdc8c337423200869e8f3daa5634c404
Sundeep <sundeep.kaler@gmail.com>
2024-09-11 22:46:05 -0400

    Complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,17 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    @property
+    def knowledge(self):
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, new_knowledge):
+        self._knowledge = new_knowledge
+
+    def learn(self, new_knowledge):
+        self._knowledge.append(new_knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,27 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+    
     def teach(self):
-        pass
+        index = random.randint(0, len(self._knowledge) - 1)
+        return self.knowledge[index]
+    
+    @property
+    def knowledge(self):
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, new_knowledge):
+        self._knowledge = new_knowledge
